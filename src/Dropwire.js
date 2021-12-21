97e9bf804f4b875599c115a30e03fc4247a79f7a
Nick Carducci <nmcarducci@gmail.com>
2021-12-20 19:06:37 -0500

    Update Dropwire.js

--- src/Dropwire.js
+++ src/Dropwire.js
@@ -1,64 +1,79 @@
 import React from "react";
 class Cable extends React.Component {
-  state = {};
+  constructor(props) {
+    super(props);
+    this.state = { limit: [], cache: null };
+    this.page = React.createRef();
+  }
   componentDidUpdate = (prevProps) => {
     if (this.props.scrolling !== prevProps.scrolling) {
+      const { cache } = this.state;
       const { scrollTopAndHeight, scrollTop } = this.props;
       var continuee = this.props.fwd.current;
-      if (!continuee) return;
-      var between =
-        scrollTopAndHeight - continuee.scrollTop > -100 &&
-        continuee.scrollTop - scrollTop > -100;
+      if (!continuee && !cache) return;
       clearTimeout(this.setset);
-      this.setset = setTimeout(
-        () =>
-          this.setState({
-            between
-          }),
-        1000
-      );
+      this.setset = setTimeout(() => {
+        var page = this.page.current;
+        var between =
+          page.offsetTop - scrollTop > -1000 &&
+          scrollTopAndHeight - page.offsetTop > -1000;
+        if (!continuee) continuee = cache;
+        this.setState({ cache: continuee, between }, () => {
+          if (!between) return continuee.remove();
+          if (page.children[0] !== this.state.cache)
+            page.appendChild(this.state.cache);
+        });
+      }, 1000);
     }
+  };
+  componentWillUnmount = () => {
+    clearTimeout(this.setset);
   };
   render() {
     const { between, continuee } = this.state;
     const { src, float, title, img } = this.props;
-    return !continuee || between ? (
-      src === "" ? (
-        <span ref={this.props.fwd} style={{ border: "1px gray solid" }}>
-          {title}
-        </span>
-      ) : img ? (
-        <img
-          onError={this.props.onError}
-          alt={title}
-          style={{
-            shapeOutside: "rect()",
-            float,
-            width: "200px",
-            border: 0,
-            ...this.props.style,
-            display: src === "" ? "none" : ""
-          }}
-          ref={this.props.fwd}
-          src={src}
-        />
-      ) : (
-        <iframe
-          onError={this.props.onError}
-          title={title}
-          style={{
-            shapeOutside: "rect()",
-            float,
-            width: "200px",
-            border: 0,
-            ...this.props.style,
-            display: src === "" ? "none" : ""
-          }}
-          ref={this.props.fwd}
-          src={src}
-        />
-      )
-    ) : null;
+    //const limited = limit.find((x) => x === Object.keys(this.props.fwd));
+    return (
+      <div ref={this.page}>
+        {!continuee || between ? (
+          src === "" ? (
+            <span ref={this.props.fwd} style={{ border: "1px gray solid" }}>
+              {title}
+            </span>
+          ) : img ? (
+            <img
+              onError={this.props.onError}
+              alt={title}
+              style={{
+                shapeOutside: "rect()",
+                float,
+                width: "200px",
+                border: 0,
+                ...this.props.style,
+                display: src === "" ? "none" : ""
+              }}
+              ref={this.props.fwd}
+              src={src}
+            />
+          ) : (
+            <iframe
+              onError={this.props.onError}
+              title={title}
+              style={{
+                shapeOutside: "rect()",
+                float,
+                width: "200px",
+                border: 0,
+                ...this.props.style,
+                display: src === "" ? "none" : ""
+              }}
+              ref={this.props.fwd}
+              src={src}
+            />
+          )
+        ) : null}
+      </div>
+    );
   }
 }
 export default React.forwardRef((props, ref) => <Cable fwd={ref} {...props} />);
