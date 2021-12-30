3091179fccde731220c7a41d8f376c46c60381fc
Nick Carducci <nmcarducci@gmail.com>
2021-12-29 19:18:37 -0500

    Update Dropwire.js

--- src/Dropwire.js
+++ src/Dropwire.js
@@ -18,7 +18,7 @@ import ExecutionEnvironment from "exenv";
 class Cable extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { limit: [], cache: null, mountsCount: 0 };
+    this.state = { limit: [], cache: null, mountsCount: 0, cacheStyle: "" };
     this.page = React.createRef();
     this.fwdtwe = React.createRef();
   }
@@ -53,7 +53,12 @@ class Cable extends React.Component {
         if (!continuee && !cache) return;
         this.setState(
           {
-            cache: cache ? cache : continuee.outerHTML
+            cache: cache ? cache : continuee.outerHTML,
+            cacheStyle: (cache ? cache : continuee.outerHTML)
+              .split(`style="`)[1]
+              .split(`"`)[0]
+              .replaceAll(";", `",`)
+              .replaceAll(": ", `: "`)
           },
           () => {
             if (!between) {
@@ -75,7 +80,7 @@ class Cable extends React.Component {
     }, timeou);
   };
   render() {
-    const { mount } = this.state;
+    const { mount, cacheStyle } = this.state;
     const { src, float, title, img } = this.props;
     //const limited = limit.find((x) => x === Object.keys(this.props.fwd));
     const onError = (e) => {
@@ -83,7 +88,7 @@ class Cable extends React.Component {
       this.props.onError(e);
     }; //ternaries remove the node and element; display removes the element, but not the node
     return (
-      <div ref={this.page}>
+      <div ref={this.page} style={{ cacheStyle }}>
         {mount ? (
           src === "" ? (
             <span style={{ border: "1px gray solid" }}>{title}</span>
