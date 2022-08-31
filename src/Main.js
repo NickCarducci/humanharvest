95c12153207c6834517c36d9e9f9958a3ea2d039
Nick Carducci <nmcarducci@gmail.com>
2022-08-31 19:49:15 -0400

    Create Main.js

---
+++ src/Main.js
@@ -0,0 +1,114 @@
+import React from "react";
+//import Cable from "./Dropwire"; //"react-dropwire";
+import { UAParser } from "ua-parser-js";
+import Retro from "./retro";
+
+export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    for (let i = 0; i < 250; i++) {
+      this["scrollImg" + i] = React.createRef();
+    }
+  }
+  componentDidMount = () => {
+    window.addEventListener("resize", this.refresh);
+    window.addEventListener("scroll", this.handleScroll);
+    this.refresh(true);
+  };
+  componentWillUnmount = () => {
+    clearTimeout(this.scrollTimeout);
+    clearTimeout(this.resizeTimer);
+    clearTimeout(this.check);
+    window.removeEventListener("resize", this.refresh);
+    window.removeEventListener("scroll", this.handleScroll);
+  };
+  handleScroll = (e) => {
+    if (!this.state.offScroll) {
+      const scrollTop = window.scrollY;
+      this.setState(
+        {
+          scrolling: true,
+          scrollTop
+        },
+        () => {
+          clearTimeout(this.scrollTimeout);
+          this.scrollTimeout = setTimeout(() => {
+            this.setState({
+              scrolling: false
+            });
+          }, 900);
+        }
+      );
+    }
+  };
+  refresh = (first) => {
+    const width = this.state.ios ? window.screen.availWidth : window.innerWidth;
+    if (first || Math.abs(this.state.lastWidth - width) > 0) {
+      clearTimeout(this.resizeTimer);
+      this.resizeTimer = setTimeout(() => {
+        this.setState({
+          lastWidth: width,
+          width,
+          availableHeight: this.state.ios
+            ? window.screen.availHeight - 20
+            : window.innerHeight
+        });
+      }, 600);
+    }
+  };
+
+  render() {
+    const handleScollImgError = (e) => {
+      if (e.message) {
+        console.log(e.message);
+        this.setState({ serviceCancelingImages: true, noyoutube: true });
+      }
+    };
+    const buttonStyle = {
+      border: "1px solid black",
+      color: "black",
+      backgroundColor: "rgb(220,220,220)",
+      borderRadius: "15px",
+      padding: "15px",
+      margin: "10px"
+    };
+    let arrayOfnumbers = [0];
+    const scrollnum = () => {
+      const num = arrayOfnumbers[arrayOfnumbers.length - 1] + 1;
+      arrayOfnumbers.push(num);
+      return num;
+    };
+    const setting = (s) => {
+      return {
+        backgroundColor: "rgb(32, 22, 11)",
+        position: s ? "relative" : "fixed",
+        transform: `translateX(${s ? 0 : -130}%)`,
+        zIndex: s ? "0" : "-9999"
+      };
+    };
+    const space = " ";
+    return (
+      <div>
+        <div
+          onClick={this.props.toggle}
+          style={{
+            bottom: "0px",
+            right: "0px",
+            position: "fixed",
+            cursor: "pointer"
+          }}
+        >
+          more
+        </div>
+        <Retro
+          lastWidth={Math.min(600, this.state.lastWidth)}
+          style={{
+            paddingBottom: "15px",
+            backgroundColor: "rgb(190,150,180)"
+          }}
+        />
+      </div>
+    );
+  }
+}
